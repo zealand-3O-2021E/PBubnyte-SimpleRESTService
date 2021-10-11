73cf591f4dd168182c532844151db61f3c4356da
Paulina Bubnytė <bubneja@gmail.com>
2021-10-11 15:22:57 +0200

    Send it

---
+++ Simple REST Service/Controllers/BooksController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Simple_REST_Service.Managers;
+using Library;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Simple_REST_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BooksManager _manager = new BooksManager();
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{ISPN13}")]
+        public Book Get(string ISPN13)
+        {
+            return _manager.GetByISBN13(ISPN13);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Book Post([FromBody] Book value)
+        {
+            return _manager.Add(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{ISPN13}")]
+        public Book Put(string ISPN13, [FromBody] Book value)
+        {
+            return _manager.Update(ISPN13, value);
+        }
+
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ISPN13}")]
+        public Book Delete(string ISPN13)
+        {
+            return _manager.Delete(ISPN13);
+        }
+    }
+}
---
+++ Simple REST Service/Managers/BooksManager.cs
@@ -0,0 +1,51 @@
+﻿using Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple_REST_Service.Managers
+{
+    public class BooksManager
+    {
+        public static List<Book> Data = new List<Book>
+        {
+            new Book {ISBN13 = "111", Author = "Person1", Title = "Title1", PageNumber = 11},
+            new Book {ISBN13 = "222", Author = "Person2", Title = "Title2", PageNumber = 22},
+            new Book {ISBN13 = "333", Author = "Person3", Title = "Title3", PageNumber = 33},
+        };
+        public List<Book> GetAll()
+        {
+            return new List<Book>(Data);
+        }
+
+        public Book GetByISBN13(string ISBN13)
+        {
+            return Data.Find(book => book.ISBN13.Contains(ISBN13));
+        }
+
+        public Book Add(Book newBook)
+        {
+            Data.Add(newBook);
+            return newBook;
+        }
+
+        public Book Delete(string ISBN13)
+        {
+            Book book = Data.Find(book1 => book1.ISBN13.Contains(ISBN13));
+            if (book == null) return null;
+            Data.Remove(book);
+            return book;
+        }
+
+        public Book Update(string ISBN13, Book updates)
+        {
+            Book book = Data.Find(book1 => book1.ISBN13.Contains(ISBN13));
+            if (book == null) return null;
+            book.Title = updates.Title;
+            book.PageNumber = updates.PageNumber;
+            book.Author = updates.Author;
+            return book;
+        } 
+    }
+}
